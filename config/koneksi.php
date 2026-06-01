2d90e676dee8a938e8fad3dc53e9ec8bc3e5682f
zansdecors-cloud <zansdecors@gmail.com>
2026-06-01 11:09:37 +0700

    Add database connection config

---
+++ config/koneksi.php
@@ -0,0 +1,12 @@
+<?php
+$host='localhost';
+$user='root';
+$pass='';
+$db='zans_decor';
+
+$conn = mysqli_connect($host,$user,$pass,$db);
+
+if(!$conn){
+    die('Koneksi database gagal : '.mysqli_connect_error());
+}
+?>
